e714854c96fe790fd245018cd7dfc45bf11ac04b
Zhirong Chen <zhirong.20@intl.zju.edu.cn>
2024-12-16 20:42:06 +0800

    finish build_mlp function

--- hw1/cs285/infrastructure/pytorch_util.py
+++ hw1/cs285/infrastructure/pytorch_util.py
@@ -44,10 +44,21 @@ def build_mlp(
         activation = _str_to_activation[activation]
     if isinstance(output_activation, str):
         output_activation = _str_to_activation[output_activation]
-
+    
     # TODO: return a MLP. This should be an instance of nn.Module
     # Note: nn.Sequential is an instance of nn.Module.
-    raise NotImplementedError
+    layers = []
+    in_size = input_size
+    
+    for _ in range(n_layers):
+        layers.append(nn.Linear(in_size, size))
+        layers.append(activation)
+        in_size = size
+
+    layers.append(nn.Linear(in_size, output_size))
+    layers.append(output_activation)
+
+    return nn.Sequential(*layers)
 
 
 device = None
